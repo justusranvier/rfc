31f9b6d089f4a76da36f7e047fbf3a1a67c246e5
Guilherme Salgado <gsalgado@gmail.com>
2015-04-21 14:41:28 -0300

    Turn schematest.py into a proper unit test

--- json/test/schematest.py
+++ json/test/schematest.py
@@ -1,28 +1,37 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 
 import jsonschema
 import json
+import os
+import unittest
 
-l = [
-    ('addressid', '01'),
-    ('outbailmentid', '01'),
-    ('outputlist', '01'),
-    ('seriesid', '01'),
-    ('siglist', '01'),
-    ('withdrawalstatus', '01')
-    ]
 
-for schema_name, schema_version in l:
+class TestJsonSchemaS(unittest.TestCase):
 
-    schema_file = '../schema/' + schema_name + '-' + schema_version + '.json'
-    data_file = '../data/' + schema_name + '-' + schema_version + '.json'
+    def testSchemas(self):
+        here = os.path.dirname(__file__)
+        schemas = [
+            ('addressid', '01'),
+            ('outbailmentid', '01'),
+            ('outputlist', '01'),
+            ('seriesid', '01'),
+            ('siglist', '01'),
+            ('withdrawalstatus', '01')]
+        for name, version in schemas:
+            schema_file = os.path.join(
+                here, '../schema/' + name + '-' + version + '.json')
+            data_file = os.path.join(
+                here, '../data/' + name + '-' + version + '.json')
+            with open(schema_file) as fd:
+                schema = json.load(fd)
+            with open(data_file) as fd:
+                test_data = json.load(fd)
+            error = None
+            try:
+                jsonschema.validate(test_data, schema)
+            except Exception as exc:
+                error = exc
 
-    schema = json.load(open(schema_file))
-    test_data = json.load(open(data_file))
-
-    result = jsonschema.validate(test_data,schema)
-
-    if result is not None:
-        print "Error validating %s against %s: %s" % (schema_file, data_file, result)
-    else:
-        print "Schema %s version %s valid." % (schema_name, schema_version)
+            self.assertIsNone(
+                error, "Error validating %s against %s: %s" % (schema_file,
+                    data_file, error))
